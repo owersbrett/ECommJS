bed6b174ba9f67e108bc8a0d3eb9fe25d009c27a
jmhammock <jason@arrayschool.com>
2019-12-18 09:44:28 -0700

    JS file

---
+++ app/app.js
@@ -0,0 +1,8 @@
+/**
+ * build the DOM elements needed for each of your products. Either copy and paste your code from the products/cart
+ * example OR rewrite the code below before starting to write the JavaScript that will build the DOM nodes needed.
+ * Please feel free to use the method in which you clone the template tag OR the method in which you build each DOM node
+ * with document.createElement()
+ **/ 
+
+
